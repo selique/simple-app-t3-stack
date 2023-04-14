23138595ff7bb2e8ffe823ac663430842bc4e8da
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-14 13:08:42 -0300

    Remove secret message reference from Header component
    Remove secret message logic used in Header component
    
    Remove api import used in Header component

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,19 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
-import { api } from "~/utils/api";
 import Button from "../../atoms/Button";
 const Header: React.FC = () => {
     const { data: sessionData } = useSession();
-
-    const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-        undefined, // no input
-        { enabled: sessionData?.user !== undefined },
-    );
 
     return (
         <div className="flex flex-col items-center justify-center gap-4">
         <p className="text-center text-2xl text-white">
             {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-            {secretMessage && <span> - {secretMessage}</span>}
         </p>
         <Button onClick={sessionData ? () => void signOut() : () => void signIn()}>
             {sessionData ? "Sign out" : "Sign in"}
