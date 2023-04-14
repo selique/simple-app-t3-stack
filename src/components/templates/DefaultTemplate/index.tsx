e0a15f88a64a8740eff92b431d5b3a73baed8b4d
Moisés Lopes Ferreira <judeudemolidor@hotmail.com>
2023-04-14 10:10:06 -0300

    rename: src/components/templates/DashboardTemplate -> DefaultTemplate
    Rename src/components/templates/DashboardTemplate to DefaultTemplate
    Update type DashboardProps to DefaultProps
    Update Component DashboardTemplate to DefaultTemplate
    Update export statement to DefaultTemplate

--- src/components/templates/DefaultTemplate/index.tsx
+++ src/components/templates/DefaultTemplate/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-type DashboardProps = {
+type DefaultProps = {
   children: React.ReactNode;
 };
 
-const DashboardTemplate: React.FC<DashboardProps> = ({ children, ...props }) => {
+const DefaultTemplate: React.FC<DefaultProps> = ({ children, ...props }) => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]" {...props}>
       {children}
@@ -12,4 +12,4 @@ const DashboardTemplate: React.FC<DashboardProps> = ({ children, ...props }) => 
   );
 }
 
-export default DashboardTemplate;
+export default DefaultTemplate;
